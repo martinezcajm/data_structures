9dfebf46c4cda99b3d7ea5f5bc710c08ad3209bd
U-PepePC\Pepe <martinezcajm@esat-alumni.com>
2017-11-16 18:12:20 +0100

    removed old folder

--- 1semestre/memory_node/build/compilacion_d.bat
+++
@@ -1,7 +0,0 @@
-@REM Compilación y Enlace con librería gráfica.
-@cls
-
-
-cl /nologo /Zi /GR- /EHs /W4 /MD ..\src\memory_node.c -I ..\deps
-@echo  Proceso por lotes finalizado.
-@echo -------------------------------
