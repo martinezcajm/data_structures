07075605c805df60f1f0c73b8b0f5fe5eaa430eb
U-PepePC\Pepe <martinezcajm@esat-alumni.com>
2017-11-16 18:07:34 +0100

    restructure of folders

---
+++ 1semestre/memory_node/include/common_def.h
@@ -0,0 +1,22 @@
+// common_def.h
+// Toni Barella
+// Algoritmos & Inteligencia Artificial
+// ESAT 2017/2018
+//
+#ifndef __COMMON_DEF_H__
+#define __COMMON_DEF_H__
+
+#define VERBOSE_
+
+typedef enum
+{
+	kErrorCode_Ok = 0,
+	kErrorCode_Memory = -1,
+	kErrorCode_File = -2,
+  kErrorCode_Null_Pointer_Received = -3,
+  kErrorCode_Error_Trying_To_Allocate_Memory = -4,
+  kErrorCode_Null_Data = -5,
+  kWarningCode_Ilogic_Expression = -6
+} ErrorCode;
+
+#endif // __COMMON_DEF_H__
